17ebaa2f4226ed7272a4d5ec27673142f951c4a5
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-04-14 15:46:54 +0300

    Update config_cisco_ios.py

--- config_cisco_ios.py
+++ config_cisco_ios.py
@@ -2,13 +2,11 @@ from netmiko import Netmiko
 from multiprocessing.dummy import Pool as ThreadPool
 import time
 
-#device list is added
 f_2 = open("multiple_device_list_cisco.txt","r")
 multiple_device_list = f_2.readlines()
 
 f_3 = open("Syslog_Unsuccess_Connection.txt","a")
 
-#username and password .txt are created in the code folder
 with open("user_pass.txt", "r") as f5:
     user_pass = f5.readlines()
 
@@ -21,7 +19,7 @@ for list_user_pass in user_pass:
 file1 = open("Syslog_Cisco_OK.txt", "a")
 
 def _ssh_(nodeip):
-  
+
     try:
         cisco = {
             'device_type': 'cisco_ios', 'ip': nodeip, 'username':
@@ -40,11 +38,20 @@ def _ssh_(nodeip):
     print("entered config mode")
     cisco_connect.send_command_timing("logging 10.222.246.12")
     cisco_connect.send_command_timing("exit")
-    cisco_connect.send_command_timing("write")
-    print("config done")
-    file1.write(nodeip + "\n")
-    cisco_connect.disconnect()
 
+    data_to_parse = cisco_connect.send_command_timing('show run | i 10.222.246.12')
+    output = ''.join(data_to_parse)
+    output2 = output.splitlines()
+    output3 = ''.join(output2)
+    output4 = output3.split(" ")
+    if "10.222.246.12" in output4:
+        print("config done")
+        file1.write(nodeip + "\n")
+    else:
+        print(nodeip + "config:nok")
+
+    cisco_connect.send_command_timing("write")
+    cisco_connect.disconnect()
 
 myPool = ThreadPool(100)
 result = myPool.map(_ssh_,multiple_device_list)
